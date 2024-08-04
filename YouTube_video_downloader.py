825c9c1548c4a788b06cedc65289f3d5fe09ce57
wojtek19022 <113895660+wojtek19022@users.noreply.github.com>
2024-08-04 23:19:43 +0200

    Dodanie przeniesienia plikow tymczasowych do folderu .hidden

--- YouTube_video_downloader.py
+++ YouTube_video_downloader.py
@@ -14,7 +14,6 @@ def on_progress(video_stream,total_size, bytes_remaining):
 def on_finish(video_stream,total_size):
     pass
 
-
 def Download():
     Czy_Tak = str(input("Czy chcesz pobierać pliki masowo? [Tak/Nie]: "))
     out = str(input(r"Ścieżka pod którą ma być zapisany plik: "))
@@ -22,6 +21,8 @@ def Download():
     if Czy_Tak.lower() == 'tak':
         file = str(input(r'Podaj ścieżkę pliku z listą filmów do pobrania [musi to być plik TXT z oddzielonym każdym linkiem przecinkiem]: '))
         with open(file, 'r+') as f:
+            temp_dir = create_temp_dir()
+
             for line in f:
                 line.splitlines(keepends=True)
                 videos = line.split(sep=',')
@@ -37,9 +38,9 @@ def Download():
                                                 use_oauth=True, 
                                                 allow_oauth_cache=True,)
                         youtubeObject_audio, youtubeObject_video = get_video_audio(youtubeObject)
-                        video_path, audio_path = download_video_audio(out, 
-                                             youtubeObject_audio, 
-                                             youtubeObject_video,)
+                        video_path, audio_path = download_video_audio(temp_dir, 
+                                                                    youtubeObject_audio, 
+                                                                    youtubeObject_video,)
                         merged_file_path = create_merged_file_dir(basedir= out, file= video_path)
                         combine_video_audio(vidname= video_path, 
                                             audname= audio_path, 
@@ -53,7 +54,6 @@ def Download():
                         print("An error has occurred", e, end="\n\n")
                     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-
     elif Czy_Tak.lower() == 'nie':
         http = str(input(r'Podaj ścieżkę pliku do pobrania z linkiem HTTPs: '))
 
@@ -63,7 +63,8 @@ def Download():
         logging.warning('Pobieranie pliku')
 
         try:
-            video_path, audio_path = download_video_audio(out, youtubeObject_audio, youtubeObject_video)
+            temp_dir = create_temp_dir()
+            video_path, audio_path = download_video_audio(temp_dir, youtubeObject_audio, youtubeObject_video)
             merged_file_path = create_merged_file_dir(basedir= out, file= video_path)
             combine_video_audio(vidname= video_path, 
                                 audname= audio_path, 
@@ -76,6 +77,20 @@ def Download():
 
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
+def create_temp_dir():
+    get_cwd = os.getcwd()
+    temp_dir = os.path.join(get_cwd,'.hidden')
+
+    if os.path.exists(temp_dir):
+        remove_temp_dir(temp_dir)
+
+    os.mkdir(temp_dir)
+    os.popen(f"attrib +h {temp_dir}")
+
+    return temp_dir
+
+def remove_temp_dir(temp_dir):
+    os.rmdir(temp_dir)
 
 def get_video_audio(youtubeObject):
     youtubeObject = youtubeObject.streams
@@ -84,18 +99,16 @@ def get_video_audio(youtubeObject):
 
     return youtubeObject_audio, youtubeObject_video
 
-
-def download_video_audio(out, youtubeObject_audio, youtubeObject_video):
-    youtubeObject_audio.download(out,filename_prefix="audio ")
-    youtubeObject_video.download(out,filename_prefix="video ")
+def download_video_audio(temp_dir, youtubeObject_audio, youtubeObject_video):
+    youtubeObject_audio.download(temp_dir,filename_prefix="audio ")
+    youtubeObject_video.download(temp_dir,filename_prefix="video ")
 
     filename = youtubeObject_video.default_filename
     
-    video_path = os.path.join(out, f"video {filename}")
-    audio_path = os.path.join(out, f"audio {filename}")
+    video_path = os.path.join(temp_dir, f"video {filename}")
+    audio_path = os.path.join(temp_dir, f"audio {filename}")
 
     return video_path, audio_path
-
 
 def create_merged_file_dir(basedir, file):
     name_file = os.path.basename(file).replace("video ","")
@@ -103,13 +116,11 @@ def create_merged_file_dir(basedir, file):
 
     return path
 
-
 def combine_video_audio(vidname, audname, outname, fps=30):
     my_clip = VideoFileClip(vidname)
     audio_background = AudioFileClip(audname)
     final_clip = my_clip.set_audio(audio_background)
     final_clip.write_videofile(outname,fps=fps)
-
 
 def delete_not_neccesary_file(**kwargs):
     for item in kwargs.values():
@@ -117,7 +128,6 @@ def delete_not_neccesary_file(**kwargs):
             os.remove(item)
         except FileNotFoundError:
             print("\nPlik został wcześniej usunięty", end="\n")
-
 
 if __name__ == "__main__":
     Download()
